eae8e25f5db346af2fe9168103f3f035da098f21
Vixard1337 <konrad.klimek1902@gmail.com>
2024-04-12 14:52:21 +0200

    Order Component Design changes

--- pFront/src/app/orders/orders.component.ts
+++ pFront/src/app/orders/orders.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.scss'
+  styleUrls: ['./orders.component.scss'] // Zmiana z `styleUrl` na `styleUrls`
 })
 export class OrdersComponent {
+  orders: any[];
 
+  constructor() {
+    this.orders = [
+      { type: 'Typ1', city: 'Miasto1', user: 'Użytkownik1' },
+      { type: 'Typ2', city: 'Miasto2', user: 'Użytkownik2' },
+      { type: 'Typ3', city: 'Miasto3', user: 'Użytkownik3' }
+    ];
+  }
+
+  selectOrder(order: any) {
+    alert(`Wybrałeś zlecenie: Typ zlecenia: ${order.type}, Miejscowość: ${order.city}`);
+  }
 }
